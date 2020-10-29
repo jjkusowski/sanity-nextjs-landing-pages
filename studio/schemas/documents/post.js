b53d808466da36db680ed328598704e0829c1813
jkusowsk <jkusowsk@cisco.com>
2020-10-29 09:35:05 -0700

    add post.js

---
+++ studio/schemas/documents/post.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'post',
+  type: 'docutment',
+  title: 'Page',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'content',
+      type: 'string',
+      title: 'Post Content',
+    },
+  ],
+};
